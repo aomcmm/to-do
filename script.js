d23306f11fc49199ad7d6ac6076d4ab340ec22af
aomcmm <aoife.buisness@gmail.com>
2024-07-04 06:32:31 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function addTodo() {
+    const input = document.getElementById('todo-input');
+    const todoText = input.value.trim();
+    if (todoText === '') return;
+
+    const li = document.createElement('li');
+    li.textContent = todoText;
+    li.onclick = function () {
+        this.remove();
+    };
+
+    document.getElementById('todo-list').appendChild(li);
+    input.value = '';
+}
 
